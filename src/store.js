f789bd4eabf5ffe77d1f890e1496ccdf0274af0d
sathishp-debug <sathishpkateel@gmail.com>
2026-02-09 17:35:25 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+const cartQuantity = useSelector(state => state.cart.items.reduce((total, item) => total + item.quantity, 0));
  const store = configureStore({
     reducer: {
         cart: cartReducer,
